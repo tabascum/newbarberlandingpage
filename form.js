8d8fdda5f2267af74aea923a6d3b7f75b104807d
tabascum <marques_vasco@yahoo.es>
2022-10-14 18:27:58 +0100

    Add all the styling on a dropdown menu. JS still not working

--- form.js
+++ form.js
@@ -1,34 +1,6 @@
-//First modal displaying a cut schedule form
+//toggle menu button for small screens
 
-let modal = document.getElementById("myModal");
-let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-//Second modal displaying SMS type message on pushing the "submit" button
-
-let myModal = document.getElementById("myModal2");
-let submit = document.getElementById("submit");
-
-submit.onclick = function() {
-  myModal.style.display = "block";
-}
-
-window.onclick = function(event) {
-  if (event.target == myModal) {
-    myModal.style.display = "none";
-  }
-}
+toggleButton.onclick = () => navbarLinks.classList.toggle("active");
